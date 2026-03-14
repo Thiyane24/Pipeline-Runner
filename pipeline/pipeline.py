d8cddbda5de0b4f7da914ba14137c594a44d1b13
Thiyane Xavier <thiyanexavier6@gmail.com>
2026-03-14 21:24:17 +0800

    added logger

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,3 +1,5 @@
+from pipeline.logger import Logger
+import time
 class Pipeline:
     def __init__ (self, name):
         self.name = name
@@ -8,11 +10,17 @@ class Pipeline:
         return self
         
     def run(self, data = None):
+        logger = Logger()
         result = data 
         for step in self.steps:
+            start = time.perf_counter()
+            logger.log_start(step)
             result = step.execute(result)
+            end = time.perf_counter()
+            duration = end - start 
+            logger.log_end(step, duration)
             step.status = "done"
-            
+        
         return result        
     
     def summary(self):
